c95a2e23abfbf1389435258e86d5a9875e776c9a
Oh <saintnun150@gmail.com>
2018-06-08 15:00:21 +0900

    initial1

--- SDMall/src/main/java/com/sol/mall/HomeController.java
+++ SDMall/src/main/java/com/sol/mall/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(HttpServletRequest req, HttpServletResponse res) {
 	
-		return "home";
+		return "member/loginArea";
 	}
 	
 }
