6e35c493198c22ddd03206ad19be4ce0661a6d56
Yun <mireupius@gmail.com>
2018-06-11 19:04:34 +0900

    2

---
+++ SDMall/src/main/java/com/sol/mall/goods/TestContent.java
@@ -0,0 +1,23 @@
+package com.sol.mall.goods;
+
+public class TestContent {
+	private String daumcontent;
+
+	public TestContent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TestContent(String daumcontent) {
+		super();
+		this.daumcontent = daumcontent;
+	}
+
+	public String getDaumcontent() {
+		return daumcontent;
+	}
+
+	public void setDaumcontent(String daumcontent) {
+		this.daumcontent = daumcontent;
+	}
+
+}
